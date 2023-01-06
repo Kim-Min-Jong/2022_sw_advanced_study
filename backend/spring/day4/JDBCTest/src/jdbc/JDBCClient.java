36fbbfc8bd3480eb58441f158bec869d56e62ab7
minjong kim <alswhddkdl@gmail.com>
2023-01-06 15:29:51 +0900

    day5 time2 jdbc & spring boot & shopping mall basic

--- backend/spring/day4/JDBCTest/src/jdbc/JDBCClient.java
+++ backend/spring/day4/JDBCTest/src/jdbc/JDBCClient.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 
 public class JDBCClient {
 
+	/**
+	 * @param args
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// Connection 객체 사용, 종료 => dao, DBManager
 
@@ -12,23 +17,23 @@ public class JDBCClient {
 
 		SimpleDao dao = new SimpleDaoImpl();
 
-//				SimpleDto dto = new SimpleDto("홍길동", "not null", "기본값");
-//				System.out.println( dao.insert(dto) ); // insert2()
+//				SimpleDto dto = new SimpleDto("오길동", "not null5", "기본값");
+//				System.out.println( dao.insert(dto) ); // inㅓsert2()
 
 //				SimpleDto dto = new SimpleDto(1, "홍길동", "not null", "기본값");
 //				System.out.println( dao.insertDup(dto) );
 
-				SimpleDto dto = new SimpleDto("박길동", "not null", "기본값");
-				System.out.println( dao.insertWithoutDefaultValue(dto) );
+//				SimpleDto dto = new SimpleDto("박길동", "not null", "기본값");
+//				System.out.println( dao.insertWithoutDefaultValue(dto) );
 		// insertWithoutDefaultValue
 
-//				SimpleDto dto = new SimpleDto(3, "유길동", "not null 2", "기본값2");
+//				SimpleDto dto = new SimpleDto(2, "유길동", "not null 2", "기본값2");
 //				System.out.println( dao.update(dto) );
 
-//				SimpleDto dto = new SimpleDto(3, "유길동", "not null 2", "기본값2");
+//				SimpleDto dto = new SimpleDto(2, "유길동", "not null 2", "기본값2");
 //				System.out.println( dao.delete(dto) );
 
-//				SimpleDto dto = dao.detail(5);
+//				SimpleDto dto = dao.detail(1);
 //				System.out.println(dto);
 
 //		ArrayList<SimpleDto> list = dao.list();
@@ -36,10 +41,10 @@ public class JDBCClient {
 //			System.out.println(dto);
 //		}
 
-//				ArrayList<SimpleDto> list = dao.list("ssafy");
-//				for (SimpleDto dto : list) {
-//					System.out.println(dto);
-//				}		
+				ArrayList<SimpleDto> list = dao.list("길");
+				for (SimpleDto dto : list) {
+					System.out.println(dto);
+				}		
 	}
 
 }
---
+++ backend/spring/day4/ShoppingMall/src/main/java/com/mycom/myapp/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.mycom.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@CrossOrigin(
+        // localhost:5500 과 127.0.0.1 구분
+        origins = "http://127.0.0.1:5500", // allowCredentials = "true" 일 경우, orogins="*" 는 X
+        allowCredentials = "true",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT,RequestMethod.HEAD, RequestMethod.OPTIONS}
+)
+public class HomeController {
+    @GetMapping(value = "/")
+    private String index() {
+        return "/index.html";
+    }
+
+    @GetMapping(value = "/login")
+    private String login() {
+        return "/login.html";
+    }
+
+    @GetMapping(value = "/register")
+    private String register() {
+        return "/register.html";
+    }
+
+    @GetMapping(value = "/product")
+    private String home() {
+        return "/product.html";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+        return "/login.html";
+    }
+
+}
